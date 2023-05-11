606674bb27fa4f1055bdf2ae4a95f67d3e61edc4
IM_hyungju <125863770+hyungjuIM@users.noreply.github.com>
2023-05-11 15:18:21 +0900

    회원정보 수정 90%상황 _임형주

--- back/CAMP404/src/main/webapp/resources/js/infoChange.js
+++ back/CAMP404/src/main/webapp/resources/js/infoChange.js
@@ -86,17 +86,46 @@ newNick.addEventListener("input", function () {
     const nickExp = /^[a-zA-Z0-9가-힣]{2,10}$/;
     if (nickExp.test(newNick.value)) {
 
-        nicknameMessage.innerText = "사용 가능한 닉네임입니다.";
-        nicknameMessage.classList.remove("error");
-        nicknameMessage.classList.add("confirm");
-        checkobj.newNick = true; // 유효o 기록
+        $.ajax({
+            url: "newNicknameDupCheck",  // 필수 작성 속성
+            data: { "memberNickname": newNick.value }, // 서버로 전달할 값(파라미터)
+            type: "GET", // 데이터 전달 방식(기본값 GET)
+
+            success: function (result) { // 비동기 통신 성공 시(에러 발생 X)
+
+                // 매개변수 result : Servlet에서 응답으로 출력된 데이터가 저장
+
+                if (result == 0) { // 닉네임 중복 X
+                    console.log("가능");
+                    nicknameMessage.innerText = "사용 가능한 닉네임 입니다.";
+                    nicknameMessage.classList.add("confirm");
+                    nicknameMessage.classList.remove("error");
+                    checkobj.newNick = true; // 유효 O 기록
+
+                } else { // 닉네임 중복 O
+                    console.log("불가");
+                    nicknameMessage.innerText = "이미 사용중인 닉네임 입니다.";
+                    nicknameMessage.classList.add("error");
+                    nicknameMessage.classList.remove("confirm");
+                    checkobj.newNick = false; // 유효 O 기록
+                }
+            },
+
+            error: function () { // 비동기 통신 중 에러가 발생한 경우
+                console.log("에러 발생");
+            }
+        });
+
+
+
     } else {
-        nicknameMessage.innerText = "닉네임 형식이 유효하지 않습니다";
+        nicknameMessage.innerText = "닉네임 형식이 유효하지 않습니다.";
         nicknameMessage.classList.add("error");
         nicknameMessage.classList.remove("confirm");
 
-        checkobj.newNick = false; // 유효x 기록
+        checkobj.newNick = false; // 유효 X 기록
     }
+
 });
 
 // 전화번호
@@ -131,8 +160,41 @@ newTel.addEventListener("input", function () {
         checkobj.newTel = false; // 유효x 기록
     }
 });
+function infoValidate(){
 
+    // checkObj에 있는 모든 속성을 반복 접근하여
+    // false가 하나라도 있는 경우에는 form태그 기본 이벤트 제거
 
+    let str;
+
+    for( let key  in checkobj ){ // 객체용 향상된 for문
+
+        // 현재 접근 중인 key의 value가 false인 경우
+        if( !checkobj[key] ){ 
+
+            switch(key){
+            case "newTel":     str="전화번호가"; break;
+            case "newPw":        str="비밀번호가"; break;    
+            case "newPwConfirm": str="비밀번호 확인이"; break;
+            case "newNick":  str="닉네임이"; break;
+           
+            }
+
+          
+        
+
+            str += " 유효하지 않습니다.";
+
+            alert(str);
+
+            document.getElementById(key).focus();
+            
+            return false; // form태그 기본 이벤트 제거
+        }
+    }
+    return true; // form태그 기본 이벤트 수행
+
+}
 
 
 function test() {
